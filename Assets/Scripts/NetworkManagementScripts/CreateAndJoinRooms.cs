ea7b8f758381ba3cc48244db16d95a5e9ff6595b
Valentin Oltyan <valentinoltyan@gmail.com>
2022-02-01 10:45:16 +0000

    Added basic network connection

---
+++ Assets/Scripts/NetworkManagementScripts/CreateAndJoinRooms.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public InputField inputWord;
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(inputWord.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(inputWord.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Map");
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
+    [SerializeField] GameObject cameraHolder;
+    private float verticalLookRotation;
+    private bool grounded;
+    private Vector3 smoothMoveVelocity;
+    private Vector3 moveAmount;
+
+    private PhotonView view;
+
+    private Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        view = GetComponent<PhotonView>();
+    }
+
+    void Start()
+    {
+        if (!view.IsMine)
+        {
+            Destroy(GetComponentInChildren<Camera>().gameObject);
+            Destroy(rb);
+        }
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (view.IsMine)
+        {
+            LockAndUnlockCursor();
+
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Look();
+            }
+            
+            Move();
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (view.IsMine)
+        {
+            rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+        }
+    }
+
+    void Look()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+
+        verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+        cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+
+    }
+    void LockAndUnlockCursor()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+
+    void Move()
+    {
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+    }
+    public void SetGroundedState(bool _grounded)
+    {
+        grounded = _grounded;
+    }
+
+}
