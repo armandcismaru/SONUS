ef53333646abd38fc15512baaf8f7101b7ef483a
Armand Mihai Cismaru <72386471+armandcismaru@users.noreply.github.com>
2022-02-14 14:15:18 +0200

    Map features (#37)
    * creating fire like light on wall and playing with mist particle system
    
    * added lights modeled in Maya, more fog, health box modeled in maya, led lights package adapted, scene with functional player that can shoot
    
    * pick up scripts + UI and fix door
    
    * first try
    
    * some fixes
    
    * rearranged scene and deleted unnecesarry stuff
    
    * implement logic for multiplayer for pick ups
    
    * multiplayer for pickups + delete extra files
    
    Co-authored-by: Victor <kd19421@bristol.ac.uk>
    Co-authored-by: Valentin Oltyan <valentinoltyan@gmail.com>

---
+++ Assets/Scripts/Pick Up Scripts/PickUpScript.cs
@@ -0,0 +1,55 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Different objects in the game can have different pick up types. These types of derivates need to be attached to each object in Unity.*/
+public class PickUpScript : MonoBehaviour {
+    public enum PickUpType {
+        Food,
+        Fuel,
+        Health,
+        Armor
+    };
+
+    [SerializeField] private Vector3 _rotation;
+    [SerializeField] private float _speed;
+    [SerializeField] private PickUpType _pickupType;
+
+    public PickUpType pickupType { get => _pickupType;}
+
+    public void destroyThisObject()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
+        else
+        {
+            GetComponent<PhotonView>().RPC("rpcDestroyObject", RpcTarget.MasterClient);
+        }
+    }
+
+    [PunRPC]
+    void rpcDestroyObject()
+    {
+        PhotonNetwork.Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        transform.Rotate(_rotation * _speed * Time.deltaTime);   
+    }
+
+    private void Start()
+    {
+        gameObject.tag = "PickUp";
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {
+        }
+    }
+}
