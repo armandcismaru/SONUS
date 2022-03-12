c3944a92ceb3d0c2f9b0c4bc900039ae926b53c1
georgeedward2000 <georgeedward2000@users.noreply.github.com>
2022-03-12 17:35:59 +0000

    Deploying to gh-pages from @ armandcismaru/SONUS@c2a56cdbe5477b58d81d25ef0540f9f5e5bf70c7 🚀

--- js/mainScript.js
+++ js/mainScript.js
@@ -27,10 +27,12 @@ function spawnWorker(workerURL, onReady) {
 // To display the hypothesis sent by the recognizer
 function updateHyp(hyp) {
   if (outputContainer) outputContainer.innerHTML = hyp;
-  // if (unityInstance) 
-  if (hyp) window.unityInstance.SendMessage("BridgeVoiceRecognition", "TriggerSpell", hyp);
-  console.log(hyp);
 };
+
+function updateHypSeg(hypseg) {
+  if (hypseg.length > 0 && hypseg[hypseg.length - 1].word.localeCompare("<sil>") != 0) window.unityInstance.SendMessage("BridgeVoiceRecognition", "TriggerSpell", hypseg[hypseg.length - 1].word);
+  
+}
 
 // This updates the UI when the app might get ready
 // Only when both recorder and recognizer are ready do we enable the buttons
@@ -149,8 +151,13 @@ window.onload = function() {
           if (e.data.hasOwnProperty('hyp')) {
             var newHyp = e.data.hyp;
             if (e.data.hasOwnProperty('final') &&  e.data.final) newHyp = "Final: " + newHyp;
-            updateHyp(newHyp);
+            updateHyp(newHyp, newHypSeg);
+            if (e.data.hasOwnProperty('hypseg')) {
+              var newHypSeg = e.data.hypseg;
+              updateHypSeg(newHypSeg);
+            }
           }
+          
           // This is the case when we have an error
           if (e.data.hasOwnProperty('status') && (e.data.status == "error")) {
             updateStatus("Error in " + e.data.command + " with code " + e.data.code);
@@ -189,12 +196,12 @@ stopBtn.onclick = stopRecording;
 
  // This is the list of words that need to be added to the recognizer
  // This follows the CMU dictionary format
-var wordList = [["ACTION", "AE K SH AH N"], ["ONE", "W AH N"], ["TWO", "T UW"], ["THREE", "TH R IY"], ["FOUR", "F AO R"], ["FIVE", "F AY V"], ["SIX", "S IH K S"], ["SEVEN", "S EH V AH N"], ["EIGHT", "EY T"], ["NINE", "N AY N"], ["ZERO", "Z IH R OW"], ["NEW-YORK", "N UW Y AO R K"], ["NEW-YORK-CITY", "N UW Y AO R K S IH T IY"], ["PARIS", "P AE R IH S"] , ["PARIS(2)", "P EH R IH S"], ["SHANGHAI", "SH AE NG HH AY"], ["SAN-FRANCISCO", "S AE N F R AE N S IH S K OW"], ["LONDON", "L AH N D AH N"], ["BERLIN", "B ER L IH N"], ["SUCKS", "S AH K S"], ["ROCKS", "R AA K S"], ["IS", "IH Z"], ["NOT", "N AA T"], ["GOOD", "G IH D"], ["GOOD(2)", "G UH D"], ["GREAT", "G R EY T"], ["WINDOWS", "W IH N D OW Z"], ["LINUX", "L IH N AH K S"], ["UNIX", "Y UW N IH K S"], ["MAC", "M AE K"], ["AND", "AE N D"], ["AND(2)", "AH N D"], ["O", "OW"], ["S", "EH S"], ["X", "EH K S"]];
+var wordList = [["ACTION", "AE K SH AH N"], ["WAREHOUSES", "W EH R HH AW Z IH Z"]];
 // This grammar recognizes digits
-var grammarDigits = {numStates: 1, start: 0, end: 0, transitions: [{from: 0, to: 0, word: "ACTION"}, {from: 0, to: 0, word: "ONE"}]};
+var grammarDigits = {numStates: 1, start: 0, end: 0, transitions: [{from: 0, to: 0, word: "ACTION"}, {from: 0, to: 0, word: "WAREHOUSES"}]};
 // This grammar recognizes a few cities names
-var grammarCities = {numStates: 1, start: 0, end: 0, transitions: [{from: 0, to: 0, word: "NEW-YORK"}, {from: 0, to: 0, word: "NEW-YORK-CITY"}, {from: 0, to: 0, word: "PARIS"}, {from: 0, to: 0, word: "SHANGHAI"}, {from: 0, to: 0, word: "SAN-FRANCISCO"}, {from: 0, to: 0, word: "LONDON"}, {from: 0, to: 0, word: "BERLIN"}]};
-// This is to play with beloved or belated OSes
-var grammarOses = {numStates: 7, start: 0, end: 6, transitions: [{from: 0, to: 1, word: "WINDOWS"}, {from: 0, to: 1, word: "LINUX"}, {from: 0, to: 1, word: "UNIX"}, {from: 1, to: 2, word: "IS"}, {from: 2, to: 2, word: "NOT"}, {from: 2, to: 6, word: "GOOD"}, {from: 2, to: 6, word: "GREAT"}, {from: 1, to: 6, word: "ROCKS"}, {from: 1, to: 6, word: "SUCKS"}, {from: 0, to: 4, word: "MAC"}, {from: 4, to: 5, word: "O"}, {from: 5, to: 3, word: "S"}, {from: 3, to: 1, word: "X"}, {from: 6, to: 0, word: "AND"}]};
+// var grammarCities = {numStates: 1, start: 0, end: 0, transitions: [{from: 0, to: 0, word: "NEW-YORK"}, {from: 0, to: 0, word: "NEW-YORK-CITY"}, {from: 0, to: 0, word: "PARIS"}, {from: 0, to: 0, word: "SHANGHAI"}, {from: 0, to: 0, word: "SAN-FRANCISCO"}, {from: 0, to: 0, word: "LONDON"}, {from: 0, to: 0, word: "BERLIN"}]};
+// // This is to play with beloved or belated OSes
+// var grammarOses = {numStates: 7, start: 0, end: 6, transitions: [{from: 0, to: 1, word: "WINDOWS"}, {from: 0, to: 1, word: "LINUX"}, {from: 0, to: 1, word: "UNIX"}, {from: 1, to: 2, word: "IS"}, {from: 2, to: 2, word: "NOT"}, {from: 2, to: 6, word: "GOOD"}, {from: 2, to: 6, word: "GREAT"}, {from: 1, to: 6, word: "ROCKS"}, {from: 1, to: 6, word: "SUCKS"}, {from: 0, to: 4, word: "MAC"}, {from: 4, to: 5, word: "O"}, {from: 5, to: 3, word: "S"}, {from: 3, to: 1, word: "X"}, {from: 6, to: 0, word: "AND"}]};
 var grammars = [{title: "Digits", g: grammarDigits}];
 var grammarIds = [];
