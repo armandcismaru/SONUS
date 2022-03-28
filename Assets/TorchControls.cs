b4c0d919579b9678f003c89cac35ff766cb2519e
Alice <nj19721@bristol.ac.uk>
2022-03-28 13:15:16 +0100

    Gave torch public on/off subroutines

--- Assets/TorchControls.cs
+++ Assets/TorchControls.cs
@@ -9,21 +9,32 @@ public class TorchControls : MonoBehaviour
     const int MAXLIGHTINTENSITY = 7;
     private float LifeRemaining = MAXTORCHLIFE;
     
-    // Start is called before the first frame update
     void Start()
     {
         TorchOn = false;
     }
 
-    // Update is called once per frame
+    public void PowerTorch()
+    {
+        TorchOn = true;
+        LifeRemaining = MAXTORCHLIFE;
+    }
+
+    public void DisableTorch()
+    {
+        TorchOn = false;
+    }
+
     void Update()
     {    
+        //To be deleted once torch is linked with voice recognition
         if (Input.GetKey(KeyCode.T))
         {
             TorchOn = !TorchOn;
             LifeRemaining = MAXTORCHLIFE;
         }
 
+        //Updates torch's "battery"
         if (TorchOn)
         {
             if (LifeRemaining > 0)
