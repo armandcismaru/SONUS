1a4ed1d407358431432bd5ac48e95a8a0b1329ec
Vlad Bucur <vladbucur@Vlads-MacBook-Pro-2.local>
2022-05-03 00:22:14 +0300

    added intro video + scene

---
+++ Assets/VideoController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class VideoController : MonoBehaviour
+{   
+    public double time;
+    public double currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = gameObject.GetComponent<VideoPlayer> ().clip.length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        currentTime = gameObject.GetComponent<VideoPlayer> ().time;
+        if (currentTime >= 10) {
+            PhotonNetwork.LoadLevel(1);
+        }
+    }
+}
